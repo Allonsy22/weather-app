e6e58c9d7b7a51be8a0cf083fde6f961bd98e1a2
Alexander Grits <a.g.allonsy@gmail.com>
2021-07-09 10:56:43 +0300

    updated notFoundPage

--- src/views/NotFoundPage.vue
+++ src/views/NotFoundPage.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="container">
-    Page not found
-    <v-btn to="/">Go Home</v-btn>
+  <div class="overlay">
+    <div class="overlay-inner">
+      Page not found
+      <v-btn
+        to="/"
+        small
+        text
+        class="ml-2"
+      >
+        <v-icon size="35">mdi-home-outline</v-icon>
+      </v-btn>
+    </div>
   </div>
 </template>
 
@@ -11,3 +20,23 @@ export default {
 
 }
 </script>
+
+<style scoped>
+.overlay {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: rgb(255,255,255);
+  background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(94,82,255,1) 100%);
+}
+
+.overlay-inner {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
